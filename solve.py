26c7d7099b7467840cebd868439445ebefe1b5b6
SimingFeng <simingrox@gmail.com>
2023-01-07 10:33:02 +0000

    initial setup

---
+++ solve.py
@@ -0,0 +1,7 @@
+def filter_words(guesses, results, words):
+    """Return a list of words that match the given guesses and results."""
+    pass
+
+def next_guess(words):  
+    """Return the next guess for the game."""
+    pass
