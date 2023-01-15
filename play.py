9e28a55f68520ac70864f331114c3b7a26563a49
SimingFeng <simingrox@gmail.com>
2023-01-16 03:48:34 +0800

    allows user to make guesses from all valid guesses list, but program will recommend guesses from valid answers list to reduce runtime

--- play.py
+++ play.py
@@ -1,8 +1,9 @@
-from data import get_wordle_answers, get_wordle_guesses
+from data import get_wordle_guesses, get_wordle_answers
 from solve import filter_answers, next_best_guesses, value
 
-guesses = get_wordle_answers()
-answers = get_wordle_answers()
+valid_user_guesses = get_wordle_guesses() # all valid guesses user can make
+program_guesses = get_wordle_answers() # guesses the program can recommend, otherwise program takes too long to run
+answers = get_wordle_answers() # will be filtered so can't use same list as program_guesses
 
 def valid_result(result):
     if len(result) != 5:
@@ -14,6 +15,9 @@ def valid_result(result):
 
 if __name__ == "__main__":
     guess = str(input("What is your initial guess?\n"))
+    while guess not in valid_user_guesses:
+        print("Invalid Guess, Try Again")
+        guess = str(input("What is your initial guess?\n"))
     print()
     for i in range(6):
         print("Turn", i + 1)
@@ -27,7 +31,7 @@ if __name__ == "__main__":
         if len(answers) == 1:
             print("The answer is \"" + answers[0] + "\"")
             break
-        best_guesses = next_best_guesses(guesses, answers)
+        best_guesses = next_best_guesses(program_guesses, answers)
         val = round(value(best_guesses[0], answers) / len(answers), 2)
         print("Number of possible answers left:", len(answers))
         if len(answers) <= 10:
@@ -39,7 +43,7 @@ if __name__ == "__main__":
             print("The best guesses are", best_guesses)
             print("These guesses are expected to eliminate", val, "possible answers")
         guess = str(input("Next guess: "))
-        while guess not in guesses:
+        while guess not in valid_user_guesses:
             print("Invalid Guess, Try Again")
             guess = str(input("Next guess: "))
         print()
